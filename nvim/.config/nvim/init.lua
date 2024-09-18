d8dc618983536cc8032dc425099988462930ed6f
DistantJragon <jacobgoodwin2017@gmail.com>
2024-09-18 11:07:53 -0400

    Separated nvim init.lua into further script files

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,50 +4,6 @@ require("host-programs")
 -- Set the leader key (needs to be set before any mappings, even plugin mappings)
 vim.g.mapleader = " "
 
-vim.g.djn_debug_mode = false
-
 require("lazy-init")
 require("keybindings")
-
--- Set options
-local set = vim.opt
-
-local tab_size = 2
-local text_window_width = 120
-
--- show matching brackets and parentheses
-set.showmatch = true
--- case insensitive searching with / key
-set.ignorecase = true
--- number of columns occupied by a tab
-set.tabstop = tab_size
--- multiple spaces act like tab-stops so the Backspace key deletes multiple spaces
-set.softtabstop = tab_size
--- converts tabs to white space
-set.expandtab = true
--- width for auto-indents
-set.shiftwidth = tab_size
--- add line numbers
-set.number = true
--- set a column border for good coding style
-set.colorcolumn = tostring(text_window_width)
--- use system clipboard when yanking and pasting
-set.clipboard = "unnamedplus"
--- highlight current cursor line
-set.cursorline = true
--- wrap long lines once they pass the highlighted column
-set.textwidth = text_window_width
--- how many lines must surround the cursor before the window scrolls
-set.scrolloff = 5
--- shows line # and character # in the status bar (n/a for lua-statusline)
-set.ruler = true
--- new horizontal split windows appear below
-set.splitbelow = true
--- new vertical split windows appear to the right
-set.splitright = true
--- specify special characters
-set.listchars = { trail = "•", nbsp = "•" }
--- show special characters
-set.list = true
--- save session options (removed saving folds b/c it was causing errors and ufo does it anyway)
-set.sessionoptions = "blank,buffers,curdir,help,tabpages,winsize,terminal"
+require("nvim-options")
--- nvim/.config/nvim/lua/host-programs.lua
+++ nvim/.config/nvim/lua/host-programs.lua
@@ -1,5 +1,16 @@
-if vim.fn.has("win32") == 1 then
-  -- vim.g.python3_host_prog = "C:/Program Files/Python/Python312/python.exe"
-else
-  vim.g.python3_host_prog = "/usr/bin/python3"
-end
+-- Providers can be disabled by setting the loaded_<name>_provider variable to 0.
+-- You can set a specific path to a provider if you please. It may be needed if, for example, you have multiple versions
+-- of python installed on your system.
+-- Otherwise, nvim will try to find the provider in your PATH.
+
+-- vim.g.loaded_node_provider = 0
+-- vim.g.node_host_prog = '/path/to/node'
+
+-- vim.g.loaded_perl_provider = 0
+-- vim.g.perl_host_prog = '/path/to/perl'
+
+-- vim.g.loaded_python_provider = 0
+-- vim.g.python3_host_prog = '/path/to/python3'
+
+-- vim.g.loaded_ruby_provider = 0
+-- vim.g.ruby_host_prog = '/path/to/ruby'
