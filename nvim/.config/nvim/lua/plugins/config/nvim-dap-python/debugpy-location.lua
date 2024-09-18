c7b8de9cfd1b51512141586ff4ace532c522803a
DistantJragon <jacobgoodwin2017@gmail.com>
2024-09-18 13:58:14 -0400

    nvim nvim-dap-python should use the debugpy mason package now (hopefully it works)

--- nvim/.config/nvim/lua/plugins/config/nvim-dap-python/debugpy-location.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-dap-python/debugpy-location.lua
@@ -1,26 +1,23 @@
-local debugpy_location = vim.fn.stdpath("data") .. "/debugpy/bin/python"
+local debugpy_location = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv"
 local ignore_debugpy_error = false
 if vim.fn.has("win32") == 1 then
-  debugpy_location = vim.fn.stdpath("data") .. "/debugpy/Scripts/python.exe"
+  debugpy_location = debugpy_location .. "/Scripts/python.exe"
+else
+  debugpy_location = debugpy_location .. "/bin/python"
 end
--- debugpy_location = "Uncomment this line and write the path to the debugpy executable here if you don't want debugpy in nvim's data folder"
+-- debugpy_location = "Uncomment this line and write the path to the debugpy executable here if you don't want to"
+--   .. "use the debugpy mason package"
 if vim.fn.executable(debugpy_location) == 0 then
   if not ignore_debugpy_error then
     vim.notify(
       "Debugpy not found at "
         .. debugpy_location
-        .. "\nPlease create a virtual environment and install debugpy with pip in "
-        .. vim.fn.stdpath("data")
+        .. "\nThis means the debugpy package is not installed in the mason package manager"
+        .. "\n(Or you have set debugpy_location to an incorrect path)"
         .. "\nYou won't be able to debug python code in neovim until this is resolved."
         .. "\nYou can suppress this message by setting ignore_debugpy_error = true in "
         .. vim.fn.stdpath("config")
         .. "lua/utils/debugpy-location.lua"
-        .. "\nCommands:"
-        .. "\npython -m venv "
-        .. vim.fn.stdpath("data")
-        .. "/debugpy ; "
-        .. vim.fn.stdpath("data")
-        .. "/debugpy/bin/python -m pip install debugpy"
         .. "\n",
       vim.log.levels.WARN
     )
